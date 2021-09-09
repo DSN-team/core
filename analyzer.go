47f438252b78a285735ef5643432adf0c9d12dd1
Eszdman <eszdman2015@gmail.com>
2021-09-10 01:07:37 +0300

    bugfixes

--- analyzer.go
+++ analyzer.go
@@ -9,6 +9,7 @@ import (
 	"net"
 	"os"
 	"reflect"
+	"runtime"
 	"strings"
 	"unsafe"
 )
@@ -25,8 +26,10 @@ type databaseStr struct {
 var dataStr = &databaseStr{}
 var dataStrInput = &databaseStr{}
 var connClient net.Conn
-var workingEnv jni.Env
-var workingClazz jni.Jclass
+var serverReader *bufio.Reader
+var callBackBufferPtr unsafe.Pointer
+var callBackBufferCap int
+var workingVM jni.VM
 var address string
 
 func fakeClient() {
@@ -39,7 +42,7 @@ func fakeClient() {
 
 	clientReader := bufio.NewReader(os.Stdin)
 
-	serverReader := bufio.NewReader(connClient)
+	serverReader = bufio.NewReader(connClient)
 	for {
 		// Waiting for the client request
 		clientRequest, err := clientReader.ReadString('\n')
@@ -75,8 +78,8 @@ func fakeClient() {
 }
 func main() {
 	address = ":8080"
-	go Java_com_dsnteam_dsn_CoreManager_runClient(0, 0)
-	fakeClient()
+	go analyzer()
+	go fakeClient()
 }
 
 //export Java_com_dsnteam_dsn_CoreManager_connectionTarget
@@ -86,10 +89,16 @@ func Java_com_dsnteam_dsn_CoreManager_connectionTarget(env uintptr, clazz uintpt
 
 //export Java_com_dsnteam_dsn_CoreManager_runClient
 func Java_com_dsnteam_dsn_CoreManager_runClient(env uintptr, clazz uintptr) {
+	println("envrunclient:", env)
 	if env != 0 {
-		workingEnv = jni.Env(env)
+		workingVM, _ = jni.Env(env).GetJavaVM()
 	}
-	go fakeClient()
+	go func() {
+		connClient, _ = net.Dial("tcp", address)
+		serverReader = bufio.NewReader(connClient)
+
+	}()
+	//go fakeClient()
 }
 func analyzer() {
 	ln, err := net.Listen("tcp", address)
@@ -108,13 +117,22 @@ func analyzer() {
 }
 
 //Инициализировать структуры и подключение
+
 //export Java_com_dsnteam_dsn_CoreManager_runAnalyzer
 func Java_com_dsnteam_dsn_CoreManager_runAnalyzer(env uintptr, clazz uintptr) {
+	println("envrun:", env)
 	if env != 0 {
-		workingEnv = jni.Env(env)
+		workingVM, _ = jni.Env(env).GetJavaVM()
 	}
 
 	go analyzer()
+}
+
+//export Java_com_dsnteam_dsn_CoreManager_writeCallBackBuffer
+func Java_com_dsnteam_dsn_CoreManager_writeCallBackBuffer(env uintptr, clazz uintptr, jbuffer uintptr) {
+	//callBackBuffer = jbuffer
+	callBackBufferPtr = jni.Env(env).GetDirectBufferAddress(jbuffer)
+	callBackBufferCap = jni.Env(env).GetDirectBufferCapacity(jbuffer)
 }
 
 func handleConnection(con net.Conn) {
@@ -126,7 +144,6 @@ func handleConnection(con net.Conn) {
 		// Waiting for the client request
 		println("reading")
 		clientRequest, err := clientReader.ReadString('\n')
-		println("request0:", clientRequest)
 		switch err {
 		case nil:
 			clientRequest := strings.TrimSpace(clientRequest)
@@ -143,7 +160,6 @@ func handleConnection(con net.Conn) {
 			log.Printf("error: %v\n", err)
 			return
 		}
-		println("request:", clientRequest)
 		// Responding to the client request
 		dataStr.io = []byte(clientRequest)
 		updateCall()
@@ -156,6 +172,7 @@ func handleConnection(con net.Conn) {
 
 //export Java_com_dsnteam_dsn_CoreManager_writeBytes
 func Java_com_dsnteam_dsn_CoreManager_writeBytes(env uintptr, clazz uintptr, inBuffer uintptr) {
+	println("envwrite:", env)
 	point := jni.Env(env).GetDirectBufferAddress(inBuffer)
 	size := jni.Env(env).GetDirectBufferCapacity(inBuffer)
 
@@ -167,12 +184,43 @@ func Java_com_dsnteam_dsn_CoreManager_writeBytes(env uintptr, clazz uintptr, inB
 	for i := 0; i < size; i++ {
 		data[i] = dataStrInput.io[i]
 	}
-	println("inputstr:", string(data))
+	runtime.KeepAlive(dataStrInput.io)
+	println("inputstr:", string(dataStrInput.io))
+	clientRequest := string(dataStrInput.io)
+	var err error
+	switch err {
+	case nil:
+		clientRequest := strings.TrimSpace(clientRequest)
+		if _, err = connClient.Write([]byte(clientRequest + "\n")); err != nil {
+			log.Printf("failed to send the client request: %v\n", err)
+		}
+	case io.EOF:
+		log.Println("client closed the connection")
+		return
+	default:
+		log.Printf("client error: %v\n", err)
+		return
+	}
+
+	// Waiting for the server response
+	serverResponse, err := serverReader.ReadString('\n')
+
+	switch err {
+	case nil:
+		log.Println(strings.TrimSpace(serverResponse))
+	case io.EOF:
+		log.Println("server closed the connection")
+		return
+	default:
+		log.Printf("server error: %v\n", err)
+		return
+	}
 	_, _ = fmt.Fprint(connClient, dataStrInput.io)
 }
 
 //export Java_com_dsnteam_dsn_CoreManager_exportBytes
 func Java_com_dsnteam_dsn_CoreManager_exportBytes(env uintptr, clazz uintptr) uintptr {
+	println("envexport:", env)
 	buffer := jni.Env(env).NewDirectByteBuffer(unsafe.Pointer(&dataStr.io), len(dataStr.io))
 	return buffer
 }
@@ -180,10 +228,30 @@ func Java_com_dsnteam_dsn_CoreManager_exportBytes(env uintptr, clazz uintptr) ui
 //Realisation for platform
 func updateCall() {
 	//Call Application to read structure and update internal data interpretations, update UI.
-
+	var curenv jni.Env
+	curenv, _ = workingVM.AttachCurrentThread()
 	//Test
 	println((dataStr.io))
-	classinput := workingEnv.FindClass("com/dsnteam/dsn/CoreManager")
-	methodid := workingEnv.GetStaticMethodID(classinput, "getUpdateCallBack", "(L)")
-	workingEnv.CallStaticObjectMethodA(classinput, methodid)
+	println("WorkingEnv:", curenv)
+	classinput := curenv.FindClass("com/dsnteam/dsn/CoreManager")
+	println("classinput:", classinput)
+	methodid := curenv.GetStaticMethodID(classinput, "getUpdateCallBack", "()V")
+	println("MethodID:", methodid)
+	var bdata []byte
+
+	sh := (*reflect.SliceHeader)(unsafe.Pointer(&bdata))
+	sh.Data = uintptr(callBackBufferPtr)
+	sh.Len = callBackBufferCap
+	sh.Cap = callBackBufferCap
+
+	println("buffer pointer:", callBackBufferPtr)
+	for i := 0; i < len(bdata); i++ {
+		if i < len(dataStr.io) {
+			bdata[i] = dataStr.io[i]
+		}
+	}
+	println("buffer write done")
+	curenv.CallStaticVoidMethodA(classinput, methodid)
+	workingVM.DetachCurrentThread()
+	runtime.KeepAlive(bdata)
 }
